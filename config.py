192b5c0b2d4c070de9112576b1e7d9b9d0c19b36
KevinTripi <103606291+KevinTripi@users.noreply.github.com>
2025-04-13 19:40:00 -0400

    google-branch

---
+++ config.py
@@ -0,0 +1,7 @@
+ALLOWED_EMAILS = {
+    "kevin.tripi1@gmail.com",
+    "tom@gmail.com",
+    "mot@gmail.com"
+}
+
+SESSION_SECRET = "9302c90e72f4c658a5946219cd60db47c170ccaa4a01e553a93902878f84e580"
